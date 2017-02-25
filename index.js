6bafd17a2b9692707ce3e67573ff2dcd91506a92
TeleCloud <Support@tele-cloud.me>
2017-02-25 22:47:37 +0200

    added push config

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ var api = new ParseServer({
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse'  // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  push: {
+      android: {
+        senderId: 'your send id',
+        apiKey: 'your app key'
+      },
+      ios: {
+        pfx: 'certs/Certificates.p12',
+        bundleId: 'com.zahr.handrider',
+        production: false
+      }
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
