874d8ffa893a7fd13e25eb19004e68c83bae6391
Ali Zahr <ali_zahr@hotmail.com>
2017-04-20 00:12:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  clientKey: process.env.CLIENT_KEY || '',
   push: {
       android: {
         senderId: process.env.GCM_Sender_id,
